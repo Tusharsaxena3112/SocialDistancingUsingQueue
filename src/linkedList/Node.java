27bccf193916d802cbb9c3d1ef2de4fad1eaeb8e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-04-01 15:37:49 +0530

    adding getter  and setters

--- src/linkedList/Node.java
+++ src/linkedList/Node.java
@@ -6,6 +6,22 @@ public class Node {
     private Customer data;
     private Node next;
 
+    public Customer getData() {
+        return data;
+    }
+
+    public void setData(Customer data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
     public Node (Customer data){
         this.data = data;
         this.next = null;
