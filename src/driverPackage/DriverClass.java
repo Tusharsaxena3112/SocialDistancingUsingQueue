2213fd2e63b24ca7623124686762e9c7689e096b
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-04-01 16:37:43 +0530

    adding mobile Number constraints

--- src/driverPackage/DriverClass.java
+++ src/driverPackage/DriverClass.java
@@ -1,5 +1,28 @@
 package driverPackage;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
 public class DriverClass {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the essentials details to enter into the market" +'\n');
+        System.out.println("What is your name");
+        String name = scanner.nextLine();
+        scanner.next();
+        System.out.println("Enter your gender in a single character:");
+        char gender = scanner.next().charAt(0);
+        System.out.println("Enter your mobile number: It must be in the form 91-XXXXXXXXXX");
+        String mobNumber = scanner.nextLine();
+        while(true){
+            if(mobNumber.matches("^91-[1-9][0-9]{9}$")){
+                break;
+            }
+            else{
+                System.out.println("Enter your phone number again: your mob number should not start with 0");
+            }
+        }
+
+    }
 }
