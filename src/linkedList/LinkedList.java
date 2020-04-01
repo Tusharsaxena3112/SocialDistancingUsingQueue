14409abcde49465703b37b258b8068a2752c5800
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-04-01 15:45:03 +0530

    adding addLast() in LinkedList.java

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,21 @@
 package linkedList;
 
+import customer.Customer;
+
 public class LinkedList {
+    private Node first;
+    private Node last;
+
+    public void addLast(Customer data) {
+        Node node  = new Node(data);
+        if(first==null){
+            first=last = node;
+        }
+        else{
+            last.setNext(node);
+            last = node;
+        }
+    }
+
 
 }
