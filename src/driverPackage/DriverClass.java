ff05b4790fa3de35564b9d163a3486d69d273a3d
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-04-01 23:03:09 +0530

    completing Formatting

--- src/driverPackage/DriverClass.java
+++ src/driverPackage/DriverClass.java
@@ -8,13 +8,11 @@ public class DriverClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the essentials details to enter into the market" +'\n');
-        System.out.println("What is your name");
+        System.out.println("What is your name?");
         String name = scanner.nextLine();
-        scanner.next();
-        System.out.println("Enter your gender in a single character:");
-        char gender = scanner.next().charAt(0);
         System.out.println("Enter your mobile number: It must be in the form 91-XXXXXXXXXX");
-        String mobNumber = scanner.nextLine();
+        String mobNumber = scanner.nextLine().trim();
+        System.out.println(mobNumber.matches("^91-[1-9][0-9]{9}$"));
         while(true){
             if(mobNumber.matches("^91-[1-9][0-9]{9}$")){
                 break;
@@ -24,7 +22,10 @@ public class DriverClass {
             }
             mobNumber = scanner.nextLine();
         }
+        System.out.println("Enter your gender in a single character:");
+        char gender = scanner.next().charAt(0);
         System.out.println("Enter your city:");
+        scanner.next();
         String city = scanner.nextLine();
         System.out.println("Whether you are infected or not: true or false as it is.");
         boolean isInfected = scanner.nextBoolean();
