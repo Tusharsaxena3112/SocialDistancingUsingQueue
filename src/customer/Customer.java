1edfe04ee5acb0cb0620d0a5177afc51a9cb63e7
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2020-04-01 15:29:51 +0530

    adding properties and constructor

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,4 +1,34 @@
 package customer;
 
+
+//        String name
+//        Character gender
+//        String mobileNumber
+//        String location / city
+//        Boolean isInfected
+//        Boolean isAwareOfInfection
+//        Boolean isAwareOfSocialDistancing
+
 public class Customer {
+    private String name;
+    private char gender;
+    private String mobileNumber;
+    private String city;
+    private boolean isInfected;
+    private boolean isAwareOfInfection;
+    private boolean isAwareOfSocialDistancing;
+
+    //Constructor
+
+    public Customer(String name, char gender, String mobileNumber, String city,
+                    boolean isInfected, boolean isAwareOfInfection,
+                    boolean isAwareOfSocialDistancing) {
+        this.name = name;
+        this.gender = gender;
+        this.mobileNumber = mobileNumber;
+        this.city = city;
+        this.isInfected = isInfected;
+        this.isAwareOfInfection = isAwareOfInfection;
+        this.isAwareOfSocialDistancing = isAwareOfSocialDistancing;
+    }
 }
